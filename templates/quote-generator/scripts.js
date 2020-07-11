eff674a184c5109d091eb034a6ce62cd5a43d71d
Jorgen Beliaev <ybeliaev@gmail.com>
2020-07-11 19:50:12 +0300

    data response

--- templates/quote-generator/scripts.js
+++ templates/quote-generator/scripts.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 // Get Quote From API
 async function getQuote() {
     // We need to use a Proxy URL to make our API call in order to avoid a CORS error
@@ -6,10 +12,23 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data)
+        console.dir(data)
+        // Check if Author field is blank and replace it with 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Гвидон Вишневский';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Dynamically reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
     } catch (error) {
         getQuote();
-        console.log("Опс..нет цитаты!", error)
+        console.log("Упс..нет цитаты!", error)
     }
 }
 
