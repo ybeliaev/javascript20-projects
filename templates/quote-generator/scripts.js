c619a3dd1e06d8675c7ff6622f79784879e8d447
Jorgen Beliaev <ybeliaev@gmail.com>
2020-07-13 17:07:08 +0300

    twitter  add

--- templates/quote-generator/scripts.js
+++ templates/quote-generator/scripts.js
@@ -4,15 +4,30 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
+
+// Loading Spinner Shown
+// function loading() {
+//     loader.hidden = false;
+//     quoteContainer.hidden = true;
+// }
+
+// Remove Loading Spinner
+// function complete() {
+//     if (!loader.hidden) {
+//         quoteContainer.hidden = false;
+//         loader.hidden = true;
+//     }
+// }
+
 // Get Quote From API
 async function getQuote() {
+    // loading();
     // We need to use a Proxy URL to make our API call in order to avoid a CORS error
     const proxyUrl = 'https://jacinto-cors-proxy.herokuapp.com/';
     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.dir(data)
         // Check if Author field is blank and replace it with 'Unknown'
         if (data.quoteAuthor === '') {
             authorText.innerText = 'Гвидон Вишневский';
@@ -26,11 +41,24 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
+        // Stop Loading, Show Quote
+        // complete();
     } catch (error) {
         getQuote();
-        console.log("Упс..нет цитаты!", error)
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+console.log(newQuoteBtn, twitterBtn)
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
