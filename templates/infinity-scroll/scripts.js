54fb4fa2bde5011bedc0461fbe71891dc0a88994
Jorgen Beliaev <ybeliaev@gmail.com>
2020-07-14 19:12:45 +0300

    scroll infinit 1 зфке

--- templates/infinity-scroll/scripts.js
+++ templates/infinity-scroll/scripts.js
@@ -1,1 +1,17 @@
-console.log(11111)
+// Unsplash API
+const count = 30;
+const apiKey = 'cruZ_r6yEZ3Se3XnlbbcwG3DTJ2SvhG-eXT-6z3sz8Q';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data)
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getPhotos();
