dec282ed4ff5daa65289ff476a729276ca6e244a
Jorgen Beliaev <ybeliaev@gmail.com>
2020-07-16 13:40:05 +0300

    displayPhotos full

--- templates/infinity-scroll/scripts.js
+++ templates/infinity-scroll/scripts.js
@@ -12,7 +12,24 @@ const apiKey = 'cruZ_r6yEZ3Se3XnlbbcwG3DTJ2SvhG-eXT-6z3sz8Q';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos() {
-
+    photosArray.forEach(photo => {
+        // Create <a> to link to full photo
+        const item = document.createElement("a");
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        // Create <img> for photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        // Put <img> inside <a>, then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    })
 }
 // Get photos from Unsplash API
 async function getPhotos() {
