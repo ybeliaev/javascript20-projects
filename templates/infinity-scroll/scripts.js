4f7e7b8157a1be189a177d9214a5e901d4edbb12
Jorgen <ybeliaev@gmail.com>
2020-07-18 11:07:04 +0300

    test

--- templates/infinity-scroll/scripts.js
+++ templates/infinity-scroll/scripts.js
@@ -1,43 +1,45 @@
+console.log("Начало работы")
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-let ready = false;
-let imagesLoaded = 0;
-let totalImages = 0;
+// let ready = false;
+// let imagesLoaded = 0;
+// let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
 const count = 30;
-const apiKey = 'cruZ_r6yEZ3Se3XnlbbcwG3DTJ2SvhG-eXT-6z3sz8Q';
+const apiKey = "jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek";
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 // Create Elements For Links & Photos, Add to DOM
-function displayPhotos() {
-    photosArray.forEach(photo => {
+function displayPhotos(data) {
+    console.log("displayPhotos запущено");
+    data.forEach(photo => {
         // Create <a> to link to full photo
         const item = document.createElement("a");
-        setAttributes(item, {
-            href: photo.links.html,
-            target: '_blank',
-        });
+        item.setAttribute("href", photo.links.html);
+        item.setAttribute("target", "_blank");
         // Create <img> for photo
         const img = document.createElement('img');
-        setAttributes(img, {
-            src: photo.urls.regular,
-            alt: photo.alt_description,
-            title: photo.alt_description,
-        });
+        img.setAttribute("src", photo.urls.regular);
+        img.setAttribute("alt", photo.alt_description);
+        img.setAttribute("title", photo.alt_description);
         // Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
-    })
+    });
+    console.log('Create img:', img)
 }
+
+
 // Get photos from Unsplash API
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
         const photosArray = await response.json();
         console.log(photosArray);
-        displayPhotos();
+        displayPhotos(photosArray);
+
     } catch (error) {
         // Catch Error Here
     }
@@ -45,3 +47,4 @@ async function getPhotos() {
 
 // On Load
 getPhotos();
+console.log(imageContainer)
