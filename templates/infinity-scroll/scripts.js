dbe17646e1348e6c2f30b79e1ca4100f7b5fda8f
Jorgen <ybeliaev@gmail.com>
2020-07-18 22:54:27 +0300

    scroll end

--- templates/infinity-scroll/scripts.js
+++ templates/infinity-scroll/scripts.js
@@ -3,17 +3,28 @@ const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
 let ready = false;
-// let imagesLoaded = 0;
-// let totalImages = 0;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 30;
+const count = 10;
 const apiKey = "jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek";
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {    
+    imagesLoaded++;
+    console.log("image loaded: ", imagesLoaded)
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
 // Create Elements For Links & Photos, Add to DOM
 function displayPhotos(data) {
-    console.log("displayPhotos запущено");
+    imagesLoaded = 0;
+    totalImages = photosArray.length;    
     data.forEach(photo => {
         // Create <a> to link to full photo
         const item = document.createElement("a");
@@ -24,6 +35,8 @@ function displayPhotos(data) {
         img.setAttribute("src", photo.urls.regular);
         img.setAttribute("alt", photo.alt_description);
         img.setAttribute("title", photo.alt_description);
+        // Event Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
         // Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -36,7 +49,7 @@ function displayPhotos(data) {
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const photosArray = await response.json();        
+        photosArray = await response.json();        
         displayPhotos(photosArray);
 
     } catch (error) {
@@ -44,9 +57,12 @@ async function getPhotos() {
     }
 }
 // Check to see if scrolling near bottom of page, Load More Photos
-window.addEventListener('scroll', () => {    
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 ) {      
-      getPhotos();      
+window.addEventListener('scroll', () => {  
+    console.log("ready: ", ready)  
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready ) {  
+        console.log("ready: ", ready)  
+        ready = false;  
+        getPhotos();      
     }    
   });
 
