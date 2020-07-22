d4c46705c76c501872afeecab1c4dc6f10eb9d6a
Jorgen <ybeliaev@gmail.com>
2020-07-22 10:57:12 +0300

    add mediaDivices

--- templates/picture_in_picture/scripts.js
+++ templates/picture_in_picture/scripts.js
@@ -1,1 +1,22 @@
-console.log("picture in picture start..")
+console.log("picture in picture start..");
+
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+// Предложить выбрать медиапоток, перейти к элементу видео, затем воспроизвести
+async function selectMediaStream() {
+    try {
+      const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+      videoElement.srcObject = mediaStream;
+      videoElement.onloadedmetadata = () => {
+        videoElement.play();
+      };
+    } catch (error) {
+      // Catch Error Here
+      console.log("Error! Here: ", error)
+    }
+  }
+
+  // On Load
+selectMediaStream();
