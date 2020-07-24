f1c8e5791e7026edcd531851b4daab33bebe4492
Jorgen <ybeliaev@gmail.com>
2020-07-24 23:40:51 +0300

    js start

--- templates/light_dark_mode/scripts.js
+++ templates/light_dark_mode/scripts.js
@@ -1,2 +1,17 @@
 console.log("custom title working..");
 
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Switch Theme Dynamically
+function switchTheme(e) {
+    console.log("Switch изменён", e)
+  }
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
