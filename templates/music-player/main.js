3d4d8b58b211026932ee69f92ca4c87c3d5ed4ef
Jorgen <ybeliaev@gmail.com>
2020-08-07 21:28:51 +0300

    update DOM add

--- templates/music-player/main.js
+++ templates/music-player/main.js
@@ -1,2 +1,65 @@
 console.log("work");
 
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+// Music
+const songs = [
+    {
+      name: 'music-1',
+      displayName: 'Depeche_Mode - Dangerous',
+      artist: 'Depeche_Mode',
+    },
+    {
+      name: 'music-2',
+      displayName: 'Depeche_Mode - happiest girl',
+      artist: 'Depeche_Mode',
+    },
+    {
+      name: 'jacinto-3',
+      displayName: 'duran-duran - ordinary world',
+      artist: 'Duran-Duran',
+    },
+    {
+      name: 'metric-1',
+      displayName: 'Front Row (Remix)',
+      artist: 'Metric/Jacinto Design',
+    },
+  ];
+  // Check if Playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+
+// Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+  music.pause();
+}
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+// Update DOM
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+  
