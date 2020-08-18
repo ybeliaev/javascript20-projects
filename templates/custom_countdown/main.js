b5d400bee88f70d4bb602075cec7f6380853d21d
Jorgen <ybeliaev@gmail.com>
2020-08-18 16:19:23 +0300

    data ui

--- templates/custom_countdown/main.js
+++ templates/custom_countdown/main.js
@@ -7,7 +7,7 @@ let dateEl = document.getElementById('date-picker');
 let countdownEl = document.getElementById('countdown');
 let countdownElTitle = document.getElementById('countdown-title');
 let countdownBtn = document.getElementById('countdown-button');
-let timeElements = document.querySelectorAll('span');
+let timeElements = document.querySelectorAll('span.date');
 
 let completeEl = document.getElementById('complete');
 let completeElInfo = document.getElementById('complete-info');
@@ -15,12 +15,40 @@ let completeBtn = document.getElementById('complete-button');
 
 let countdownTitle = "";
 let countdownDate = "";
-let countdownValue ;
+let countdownValue;
+
+let second = 1000;
+let minute = second * 60;
+let hour = minute * 60;
+let day = hour * 24;
 
 
 // Set Date Input Min & Value with Today's Date
 let today = new Date().toISOString().split("T")
 dateEl.setAttribute("min", today)
+
+// Populate Countdown / Complete UI
+function updateDOM(){
+  let now = new Date().getTime()
+  let distance = countdownValue - now
+  let days = Math.floor(distance/day)
+  let hours = Math.floor((days % 24) / hour)
+  let minutes = Math.floor((distance % hour) / minute)
+  let seconds = Math.floor((distance % minute) / second)
+  // Populate Countdown  
+  completeElInfo.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`
+  timeElements[1].textContent = `${hours}`
+  timeElements[2].textContent = `${minutes}`
+  timeElements[3].textContent = `${seconds}`
+  // Hide Input
+  inputContainer.hidden = true;
+  // Show Countdown
+  completeEl.hidden = true;
+  countdownEl.hidden = false;
+}
+
+
 
 // Take value from Input
 function updateCountdown(e) {
@@ -36,7 +64,7 @@ function updateCountdown(e) {
         alert("Please select of current data")
       }else{
         countdownValue = new Date(countdownDate).getTime();
-        console.log(countdownValue)
+        updateDOM()
       }
 }
 // Event Listener
