2e1ec1910b87c62656c098a461b0984fd83917d0
Jorgen <ybeliaev@gmail.com>
2020-08-29 18:56:38 +0300

    currenttime & duration

--- templates/video-player/main.js
+++ templates/video-player/main.js
@@ -35,9 +35,20 @@ function togglePlay() {
 video.addEventListener("ended", showPlayIcon);
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+  let minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  minutes = minutes > 9 ? minutes : `0${minutes}`;
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+}
+
 // Update progress bar as video plays
 function updateProgress() {
   progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)} /`;
+  duration.textContent = `${displayTime(video.duration)}`;
 }
 
 // Volume Controls --------------------------- //
