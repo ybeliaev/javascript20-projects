2065ba07011657706d5d6edf8b9b22463693627d
Jorgen <ybeliaev@gmail.com>
2020-09-02 22:13:39 +0300

    Make sure passwords match

--- templates/form_validator/main.js
+++ templates/form_validator/main.js
@@ -9,15 +9,31 @@ const messageContainer = document.querySelector(".message-container");
 const message = document.querySelector("#message");
 
 let isValid = false;
+let passwordMatch = false;
 
 function validateForm() {
   // Use HTML constraint API to check form validity
   isValid = form.checkValidity();
   console.log(isValid);
   // Style main message for an error
-  message.textContent = "Please fill out all fields.";
-  message.style.color = "red";
-  messageContainer.style.borderColor = "red";
+  if (!isValid) {
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+  }
+  // Chek to see if password match
+  if (password1El.value === password2El.value) {
+    passwordMatch = true;
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordMatch = false;
+    message.textContent = "Make sure passwords match.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+  }
 }
 
 function processFormData(e) {
