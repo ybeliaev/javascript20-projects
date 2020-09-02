6d8917c818c4c6c195cc2ab5be22d9b7fba4b1eb
Jorgen <ybeliaev@gmail.com>
2020-09-02 19:46:17 +0300

    check form change text

--- templates/form_validator/main.js
+++ templates/form_validator/main.js
@@ -2,3 +2,28 @@ console.log(
   "Form validator: ",
   new Date().getHours() + ":" + new Date().getMinutes()
 );
+const form = document.querySelector("#form");
+const password1El = document.querySelector("#password1");
+const password2El = document.querySelector("#password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.querySelector("#message");
+
+let isValid = false;
+
+function validateForm() {
+  // Use HTML constraint API to check form validity
+  isValid = form.checkValidity();
+  console.log(isValid);
+  // Style main message for an error
+  message.textContent = "Please fill out all fields.";
+  message.style.color = "red";
+  messageContainer.style.borderColor = "red";
+}
+
+function processFormData(e) {
+  e.preventDefault();
+  // Validate Form
+  validateForm();
+}
+// Event Listener
+form.addEventListener("submit", processFormData);
