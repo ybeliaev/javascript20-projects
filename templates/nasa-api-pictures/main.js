8a3f10782d89c18e8dc96c9435b9d1e8571a189f
Jorgen <ybeliaev@gmail.com>
2020-09-12 12:39:01 +0300

    component added realiz

--- templates/nasa-api-pictures/main.js
+++ templates/nasa-api-pictures/main.js
@@ -12,6 +12,7 @@ const apiKey = "DEMO_KEY";
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
 function updateDOM() {
   resultsArray.forEach((result) => {
@@ -40,6 +41,7 @@ function updateDOM() {
     const saveText = document.createElement("p");
     saveText.classList.add("clickable");
     saveText.textContent = "Add to Favorites";
+    saveText.setAttribute("onclick", `saveFavorite("${result.url}")`);
     // Card Text
     const cardText = document.createElement("p");
     cardText.textContent = result.explanation;
@@ -73,6 +75,22 @@ async function getNasaPictures() {
   } catch (error) {
     // Catch Error Here
   }
+}
+
+// Add result to favorites
+function saveFavorite(itemUrl) {
+  // Loop through results array to select Favorites
+  resultsArray.forEach((item) => {
+    if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+      favorites[itemUrl] = item;
+      console.log(favorites);
+      // Show save confirmation for 2 sec
+      saveConfirmed.hidden = false;
+      setTimeout(() => {
+        saveConfirmed.hidden = true;
+      }, 2000);
+    }
+  });
 }
 
 // On Load
