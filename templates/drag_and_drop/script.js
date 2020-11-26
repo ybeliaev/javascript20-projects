a01f0202c8cc7957b0be310b053ad1510e0f4b41
Jorgen <ybeliaev@gmail.com>
2020-11-26 15:27:52 +0200

    add item lists inital array, get from localstogage

---
+++ templates/drag_and_drop/script.js
@@ -0,0 +1,40 @@
+const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
+const saveItemBtns = document.querySelectorAll('.solid');
+const addItemContainers = document.querySelectorAll('.add-container');
+const addItems = document.querySelectorAll('.add-item');
+// Item Lists
+const listColumns = document.querySelectorAll('.drag-item-list');
+const backlogListEl = document.getElementById('backlog-list');
+const progressListEl = document.getElementById('progress-list');
+const completeListEl = document.getElementById('complete-list');
+const onHoldListEl = document.getElementById('on-hold-list');
+
+// Items
+let updatedOnLoad = false;
+
+// Initialize Arrays
+let backlogListArray = [];
+let progressListArray = [];
+let completeListArray = [];
+let onHoldListArray = [];
+let listArrays = [];
+
+// Drag Functionality
+let draggedItem;
+let dragging = false;
+let currentColumn;
+
+// Get Arrays from localStorage if available, set default values if not
+function getSavedColumns() {
+    if (localStorage.getItem('backlogItems')) {
+      backlogListArray = JSON.parse(localStorage.backlogItems);
+      progressListArray = JSON.parse(localStorage.progressItems);
+      completeListArray = JSON.parse(localStorage.completeItems);
+      onHoldListArray = JSON.parse(localStorage.onHoldItems);
+    } else {
+      backlogListArray = ['Release the course', 'Sit back and relax'];
+      progressListArray = ['Work on projects', 'Listen to music'];
+      completeListArray = ['Being cool', 'Getting stuff done'];
+      onHoldListArray = ['Being uncool'];
+    }
+  }
